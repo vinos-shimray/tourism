7ad8ae511e534710b4e3c3f766c36a87c6e64aac
vronra <vinosshimray@gmail.com>
2023-01-28 21:28:15 +0530

    Deployment preparation

--- public/js/stripe.js
+++ public/js/stripe.js
@@ -8,9 +8,7 @@ const stripe = Stripe(
 export const bookTour = async tourId => {
   try {
     // 1) Get checkout session from API
-    const session = await axios(
-      `http://localhost:8000/api/v1/bookings/checkout-session/${tourId}`
-    );
+    const session = await axios(`/api/v1/bookings/checkout-session/${tourId}`);
     console.log(session);
 
     // 2) Create checkout form + chanre credit card
@@ -18,7 +16,7 @@ export const bookTour = async tourId => {
       sessionId: session.data.session.id
     });
   } catch (err) {
-    console.log(err);
+    // console.log(err);
     showAlert('error', err);
   }
 };
